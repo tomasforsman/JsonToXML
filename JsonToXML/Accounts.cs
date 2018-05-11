d8478d5aa9fde20a7eef80c5fac896d09318f15d
tomasforsman <tomas@tomasforsman.com>
2018-05-11 13:44:27 +0200

    Remade to better suit specification and to be more adaptable. It's now possible to enter many json files as arguments.

---
+++ JsonToXML/Accounts.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace JsonToXML
+{
+
+    public class Accounts
+    {
+        public Active_Accounts[] active_accounts { get; set; }
+        public Terminated_Accounts[] terminated_accounts { get; set; }
+        public Account_List[] account_list { get; set; }
+
+        public void SaveXML(string saveName)
+        {
+            using (var stream = new FileStream(saveName, FileMode.Create))
+            {
+                XmlSerializer XML = new XmlSerializer(typeof(Accounts));
+                XML.Serialize(stream, this);
+            }
+        }
+    }
+
+    public class Active_Accounts
+    {
+        public string id { get; set; }
+        public string bban { get; set; }
+        public string iban { get; set; }
+        public Primary_Account_Owner primary_account_owner { get; set; }
+        public string currency_code { get; set; }
+        public Disposable_Amount disposable_amount { get; set; }
+        public Balance balance { get; set; }
+        public Approved_Credit_Amount approved_credit_amount { get; set; }
+        public Reserved_Amount reserved_amount { get; set; }
+        public Account_Type account_type { get; set; }
+        public string custom_name { get; set; }
+        public Actions actions { get; set; }
+    }
+
+    public class Primary_Account_Owner
+    {
+        public string personal_identity_number { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Disposable_Amount
+    {
+        public string amount { get; set; }
+        public string currency_code { get; set; }
+    }
+
+    public class Balance
+    {
+        public string amount { get; set; }
+        public string currency_code { get; set; }
+    }
+
+    public class Approved_Credit_Amount
+    {
+        public string amount { get; set; }
+        public string currency_code { get; set; }
+    }
+
+    public class Reserved_Amount
+    {
+        public string amount { get; set; }
+        public string currency_code { get; set; }
+    }
+
+    public class Account_Type
+    {
+        public string code { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Actions
+    {
+        public bool withdrawal { get; set; }
+        public bool deposit { get; set; }
+        public bool payment { get; set; }
+    }
+
+    public class Terminated_Accounts
+    {
+        public string id { get; set; }
+        public string iban { get; set; }
+        public string bban { get; set; }
+        public string termination_date { get; set; }
+    }
+
+    public class Account_List
+    {
+        public string id { get; set; }
+        public string currency { get; set; }
+        public string product { get; set; }
+        public string account_type { get; set; }
+        public string iban { get; set; }
+        public string bic { get; set; }
+        public string bban { get; set; }
+        public string clearingnumber { get; set; }
+        public string account_number { get; set; }
+        public Balance1[] balances { get; set; }
+    }
+
+    public class Balance1
+    {
+        public Booked booked { get; set; }
+    }
+
+    public class Booked
+    {
+        public Amount amount { get; set; }
+        public string date { get; set; }
+    }
+
+    public class Amount
+    {
+        public string currency { get; set; }
+        public int content { get; set; }
+    }
+
+}
--- JsonToXML/Program.cs
+++ JsonToXML/Program.cs
@@ -17,166 +17,46 @@ copies or substantial portions of the Software.
 using Newtonsoft.Json;
 using System;
 using System.IO;
-using System.Xml;
+using Newtonsoft.Json.Linq;
 
 namespace JsonToXML
 {
-
-
     class Program
     {
         static void Main(string[] args)
         {
             try
             {
-                string jsonSEB = File.ReadAllText(@"seb-accounts.json").ToString();
-                AccountsSEB accountsSEB = JsonConvert.DeserializeObject<AccountsSEB>(jsonSEB);
-
-                string jsonSwedbank = File.ReadAllText(@"swedbank-accounts.json").ToString();
-                AccountsSwedbank accountsSwedbank = JsonConvert.DeserializeObject<AccountsSwedbank>(jsonSwedbank);
-
-                using (XmlWriter writer = XmlWriter.Create("merged-accounts.xml"))
+                Accounts accounts = new Accounts();
+                JObject ot = new JObject();
+                JObject o = new JObject();
+                string saveName = "";
+                for (int i = 0; i < args.Length; i++)
                 {
-                    writer.WriteStartDocument();
-                    writer.WriteStartElement("Accounts");
-
-                    SerializeSEB(writer, accountsSEB);
-                    SerializeSwedbank(writer, accountsSwedbank);
-
-                    writer.WriteEndElement();
-                    writer.WriteEndDocument();
+                    AddJson(args, ref ot, o, i);
+                    saveName = saveName + SaveName(args, i);
                 }
-
+                accounts = JsonConvert.DeserializeObject<Accounts>(o.ToString());
+                accounts.SaveXML(saveName + DateTime.Now.ToString("yyyyMMddHHmmss") + ".xml");
 
             }
 
             catch (Exception ex)
             {
                 Console.Write(ex.Message.ToString());
-                throw ex;
+                Environment.Exit(1);
             }
-            Console.WriteLine("merged-accounts.xml created, press any key to continue...");
-            Console.ReadKey();
         }
 
-        private static void SerializeSEB(XmlWriter writer, AccountsSEB accountsSEB)
+        private static string SaveName(string[] args, int i)
         {
-            foreach (var account in accountsSEB.ActiveAccounts)
-            {
-                writer.WriteStartElement("Account");
-                writer.WriteAttributeString("id", account.Id.ToString());
-                writer.WriteAttributeString("active", "true");
-                writer.WriteElementString("iban", account.Iban);
-                writer.WriteElementString("bic", "ESSESESS");
-                writer.WriteElementString("currency", account.CurrencyCode);
-                writer.WriteElementString("accountType", account.AccountType.Text);
-                writer.WriteElementString("accountName", account.CustomName);
-                writer.WriteElementString("customerName", account.PrimaryAccountOwner.Name);
-                writer.WriteElementString("customerId", account.PrimaryAccountOwner.PersonalIdentityNumber);
-                writer.WriteElementString("clearingNumber", "N/A");
-                writer.WriteElementString("accountNumber", "N/A");
-                writer.WriteElementString("currentBalance", account.Balance.Amount);
-                writer.WriteElementString("currentCurrency", account.Balance.CurrencyCode);
-                writer.WriteElementString("availableBlanace", account.DisposableAmount.Amount);
-                writer.WriteElementString("availableCurrency", account.DisposableAmount.CurrencyCode);
-                writer.WriteElementString("creditBalance", account.ApprovedCreditAmount.Amount);
-                writer.WriteElementString("creditCurrency", account.ApprovedCreditAmount.CurrencyCode);
-                writer.WriteStartElement("availableActions");
-                if (account.Actions.Withdrawal == false)
-                {
-                    writer.WriteAttributeString("withdrawal", "false");
-                }
-                else
-                {
-                    writer.WriteAttributeString("withdrawal", "true");
-                }
-                if (account.Actions.Withdrawal == false)
-                {
-                    writer.WriteAttributeString("deposit", "false");
-                }
-                else
-                {
-                    writer.WriteAttributeString("deposit", "true");
-                }
-                if (account.Actions.Withdrawal == false)
-                {
-                    writer.WriteAttributeString("payment", "false");
-                }
-                else
-                {
-                    writer.WriteAttributeString("payment", "true");
-                }
-
-                writer.WriteEndElement();
-                writer.WriteEndElement();
-            }
-            foreach(var account in accountsSEB.TerminatedAccounts)
-            {
-                writer.WriteStartElement("Account");
-                writer.WriteAttributeString("id", account.Id.ToString());
-                writer.WriteAttributeString("active", "false");
-                writer.WriteElementString("iban", account.Iban);
-
-                writer.WriteEndElement();
-            }
-
+            return args[i].Replace(".json", "_");
         }
 
-        private static void SerializeSwedbank(XmlWriter writer, AccountsSwedbank accountsSwedbank)
+        private static void AddJson(string[] args, ref JObject ot, JObject o, int i)
         {
-            foreach (var account in accountsSwedbank.AccountList)
-            {
-                if (account.Balances != null)
-                {
-                    writer.WriteStartElement("Account");
-                    writer.WriteAttributeString("id", account.Id.ToString());
-                    writer.WriteAttributeString("active", "true");
-                    writer.WriteElementString("iban", account.Iban);
-                    writer.WriteElementString("bic", account.Bic);
-                    writer.WriteElementString("currency", account.Currency);
-                    writer.WriteElementString("accountType", account.Product);
-                    writer.WriteElementString("accountName", account.Product);
-                    writer.WriteElementString("customerName", "N/A");
-                    writer.WriteElementString("customerId", "N/A");
-                    writer.WriteElementString("clearingNumber", account.Clearingnumber);
-                    writer.WriteElementString("accountNumber", account.AccountNumber);
-                    foreach (var balance in account.Balances)
-                    {
-                        writer.WriteElementString("currentBalance", balance.Booked.Amount.Content.ToString());
-                        writer.WriteElementString("currentCurrency", balance.Booked.Amount.Currency);
-                        writer.WriteElementString("availableBlanace", balance.Booked.Amount.Content.ToString());
-                        writer.WriteElementString("availableCurrency", balance.Booked.Amount.Currency);
-                        writer.WriteElementString("creditBalance", "N/A");
-                        writer.WriteElementString("creditCurrency", "N/A");
-                    }
-                    writer.WriteStartElement("availableActions");
-                    writer.WriteAttributeString("withdrawal", "true");
-                    writer.WriteAttributeString("deposit", "true");
-                    writer.WriteAttributeString("payment", "true");
-                
-
-                    writer.WriteEndElement();
-                    writer.WriteEndElement();
-                }
-                else
-                {
-                    writer.WriteStartElement("Account");
-                    writer.WriteAttributeString("id", account.Id.ToString());
-                    writer.WriteAttributeString("active", "false");
-                    writer.WriteElementString("iban", account.Iban);
-                    writer.WriteElementString("bic", account.Bic);
-                    writer.WriteElementString("currency", account.Currency);
-                    writer.WriteElementString("accountType", account.Product);
-                    writer.WriteElementString("accountName", account.Product);
-                    writer.WriteElementString("customerName", "N/A");
-                    writer.WriteElementString("customerId", "N/A");
-                    writer.WriteElementString("clearingNumber", account.Clearingnumber);
-                    writer.WriteElementString("accountNumber", account.AccountNumber);
-
-                    writer.WriteEndElement();
-                }
-            }
-            }
+                ot = JObject.Parse(File.ReadAllText(args[i]));
+                o.Merge(ot, new JsonMergeSettings { MergeArrayHandling = MergeArrayHandling.Union });
         }
-
     }
+}
