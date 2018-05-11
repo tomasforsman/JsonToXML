34e2247dfb525b24ab271ed786231dfc72f1ccdd
tomasforsman <tomas@tomasforsman.com>
2018-05-11 13:57:50 +0200

    Refactoring

--- JsonToXML/Accounts.cs
+++ JsonToXML/Accounts.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+MIT License
 
-using System.Globalization;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
+Copyright (c) 2018 tomasforsman
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+*/
 using System.Xml.Serialization;
 using System.IO;
 
 namespace JsonToXML
 {
-
     public class Accounts
     {
         public Active_Accounts[] active_accounts { get; set; }
